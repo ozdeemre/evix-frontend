db977eec3881abb54e9e762e9d0cbe71b03f92eb
Emre ÖZDEMİR <emre@Akia.pvt>
2025-06-18 14:17:15 +0300

    Update assets and context, remove old images

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,129 +1,39 @@
-import a_k_kose_1 from './alituram (1).jpg'
-import a_k_kose_2 from './alituram (2).jpg'
-import a_k_kose_3 from './alituram (3).jpg'
-import a_k_kose_4 from './alituram (4).jpg'
-import a_k_kose_5 from './alituram (5).jpg'
-import a_k_kose_6 from './alituram (6).jpg'
-import a_k_kose_7 from './alituram (7).jpg'
-import a_k_kose_8 from './alituram (8).jpg'
-import a_k_kose_9 from './alituram (9).jpg'
-import a_k_kose_10 from './alituram (10).jpg'
-import ak_kose_1 from './ak_kose (1).jpg'
-import ak_kose_2 from './ak_kose (2).jpg'
-import ak_kose_3 from './ak_kose (3).jpg'
-import ak_kose_4 from './ak_kose (4).jpg'
-import ak_kose_5 from './ak_kose (5).jpg'
-import ak_kose_6 from './ak_kose (6).jpg'
-import ak_kose_7 from './ak_kose (7).jpg'
-import ak_kose_8 from './ak_kose (8).jpg'
-import ak_kose_9 from './ak_kose (9).jpg'
-import ak_kose_10 from './ak_kose (10).jpg'
-import ak_kose_11 from './ak_kose (11).jpg'
-import assa_1 from './assa (1).jpg'
-import assa_2 from './assa (2).jpg'
-import assa_3 from './assa (3).jpg'
-import assa_4 from './assa (4).jpg'
-import assa_5 from './assa (5).jpg'
-import assa_6 from './assa (6).jpg'
-import assa_7 from './assa (7).jpg'
-import assa_8 from './assa (8).jpg'
-import assa_9 from './assa (9).jpg'
-import assa_10 from './assa (10).jpg'
-import assa_11 from './assa (11).jpg'
-import assa_12 from './assa (12).jpg'
-import aydinmobilya_1 from './aydınmobilya (1).jpg'
-import aydinmobilya_2 from './aydınmobilya (2).jpg'
-import aydinmobilya_3 from './aydınmobilya (3).jpg'
-import aydinmobilya_4 from './aydınmobilya (4).jpg'
-import aydinmobilya_5 from './aydınmobilya (5).jpg'
-import aydinmobilya_6 from './aydınmobilya (6).jpg'
-import aydinmobilya_7 from './aydınmobilya (7).jpg'
-import aydinmobilya_8 from './aydınmobilya (8).jpg'
-import aydinmobilya_9 from './aydınmobilya (9).jpg'
-import aydinmobilya_10 from './aydınmobilya (10).jpg'
-import aydinmobilya_11 from './aydınmobilya (11).jpg'
-import aydinmobilya_12 from './aydınmobilya (12).jpg'
-import ayens_1 from './ayens (1).jpg'
-import ayens_2 from './ayens (2).jpg'
-import ayens_3 from './ayens (3).jpg'
-import ayens_4 from './ayens (4).jpg'
-import ayens_5 from './ayens (5).jpg'
-import ayens_6 from './ayens (6).jpg'
-import ayens_7 from './ayens (7).jpg'
-import ayens_8 from './ayens (8).jpg'
-import ayens_9 from './ayens (9).jpg'
-import ayens_10 from './ayens (10).jpg'
-import ayens_11 from './ayens (11).jpg'
-import ayens_12 from './ayens (12).jpg'
-import ayens_13 from './ayens (13).jpg'
-import bohem_1 from './bohem (1).jpg'
-import bohem_2 from './bohem (2).jpg'
-import bohem_3 from './bohem (3).jpg'
-import bohem_4 from './bohem (4).jpg'
-import bohem_5 from './bohem (5).jpg'
-import bohem_6 from './bohem (6).jpg'
-import bohem_7 from './bohem (7).jpg'
-import bohem_8 from './bohem (8).jpg'
-import bohem_9 from './bohem (9).jpg'
-import latte_1 from './latte (1).jpg'
-import latte_2 from './latte (2).jpg'
-import latte_3 from './latte (3).jpg'
-import latte_4 from './latte (4).jpg'
-import latte_5 from './latte (5).jpg'
-import latte_6 from './latte (6).jpg'
-import latte_7 from './latte (7).jpg'
-import latte_8 from './latte (8).jpg'
-import latte_9 from './latte (9).jpg'
-import latte_10 from './latte (10).jpg'
-import mobelax_1 from './mobelax (1).JPG'
-import mobelax_2 from './mobelax (2).JPG'
-import mobelax_3 from './mobelax (3).JPG'
-import mobelax_4 from './mobelax (4).JPG'
-import mobelax_5 from './mobelax (5).JPG'
-import mobelax_6 from './mobelax (6).JPG'
-import mobelax_7 from './mobelax (7).JPG'
-import mobelax_8 from './mobelax (8).JPG'
-import mobelax_9 from './mobelax (9).JPG'
-import mobelax_10 from './mobelax (10).JPG'
-import mobelax_11 from './mobelax (11).JPG'
-import mobelax_12 from './mobelax (12).JPG'
-import mobelax_13 from './mobelax (13).JPG'
-import odansse_1 from './odansse (1).jpg'
-import odansse_2 from './odansse (2).jpg'
-import odansse_3 from './odansse (3).jpg'
-import odansse_4 from './odansse (4).jpg'
-import odansse_5 from './odansse (5).jpg'
-import odansse_6 from './odansse (6).jpg'
-import odansse_7 from './odansse (7).jpg'
-import odansse_8 from './odansse (8).jpg'
-import odansse_9 from './odansse (9).jpg'
-import odansse_10 from './odansse (10).jpg'
-import odansse_11 from './odansse (11).jpg'
-import odansse_12 from './odansse (12).jpg'
-import odansse_13 from './odansse (13).jpg'
-import odansse_14 from './odansse (14).jpg'
-import sandi_1 from './sandi (1).jpg'
-import sandi_2 from './sandi (2).jpg'
-import sandi_3 from './sandi (3).jpg'
-import sandi_4 from './sandi (4).jpg'
-import sandi_5 from './sandi (5).jpg'
-import sandi_6 from './sandi (6).jpg'
-import sandi_7 from './sandi (7).jpg'
-import sandi_8 from './sandi (8).jpg'
-import tree_1 from './tree (1).jpg'
-import tree_2 from './tree (2).jpg'
-import tree_3 from './tree (3).jpg'
-import tree_4 from './tree (4).jpg'
-import tree_5 from './tree (5).jpg'
-import tree_6 from './tree (6).jpg'
-import tree_7 from './tree (7).jpg'
-import tree_8 from './tree (8).jpg'
-import tree_9 from './tree (9).jpg'
-import tree_10 from './tree (10).jpg'
-import tree_11 from './tree (11).jpg'
-import tree_12 from './tree (12).jpg'
-import tree_13 from './tree (13).jpg'
-import tree_14 from './tree (14).jpg'
+import a_k_kose_1 from './alituram-1.jpg'
+import a_k_kose_2 from './alituram-2.jpg'
+import a_k_kose_3 from './alituram-3.jpg'
+import a_k_kose_4 from './alituram-4.jpg'
+import ak_kose_1 from './ak_kose-1.jpg'
+import ak_kose_2 from './ak_kose-2.jpg'
+import ak_kose_3 from './ak_kose-3.jpg'
+import ak_kose_4 from './ak_kose-4.jpg'
+import assa_1 from './assa-1.jpg'
+import assa_2 from './assa-2.jpg'
+import assa_3 from './assa-3.jpg'
+import assa_4 from './assa-4.jpg'
+import aydinmobilya_1 from './aydınmobilya-1.jpg'
+import aydinmobilya_2 from './aydınmobilya-2.jpg'
+import aydinmobilya_3 from './aydınmobilya-3.jpg'
+import aydinmobilya_4 from './aydınmobilya-4.jpg'
+import ayens_1 from './ayens-1.jpg'
+import ayens_2 from './ayens-2.jpg'
+import ayens_3 from './ayens-3.jpg'
+import ayens_4 from './ayens-4.jpg'
+import bohem_1 from './bohem-1.jpg'
+import bohem_2 from './bohem-2.jpg'
+import bohem_3 from './bohem-3.jpg'
+import bohem_4 from './bohem-4.jpg'
+import latte_1 from './latte-1.jpg'
+import latte_2 from './latte-2.jpg'
+import latte_3 from './latte-3.jpg'
+import latte_4 from './latte-4.jpg'
+import sandi_1 from './sandi-1.jpg'
+import sandi_2 from './sandi-2.jpg'
+import sandi_3 from './sandi-3.jpg'
+import sandi_4 from './sandi-4.jpg'
+import tree_1 from './tree-1.jpg'
+import tree_2 from './tree-2.jpg'
+import tree_3 from './tree-3.jpg'
+import tree_4 from './tree-4.jpg'
 
 
 
@@ -174,7 +84,7 @@ export const products = [
         name: "Ali Turam Köşe Takımı",
         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
         price: 100,
-        image: [a_k_kose_1, a_k_kose_2, a_k_kose_3, a_k_kose_4, a_k_kose_5, a_k_kose_6, a_k_kose_7, a_k_kose_8, a_k_kose_9, a_k_kose_10],
+        image: [a_k_kose_1, a_k_kose_2, a_k_kose_3, a_k_kose_4],
         category: "Koltuk",
         subCategory: "Topwear",
         sizes: ["S", "M", "L"],
@@ -186,7 +96,7 @@ export const products = [
         name: "A&K Köşe Takımı",
         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
         price: 100,
-        image: [ak_kose_1, ak_kose_2, ak_kose_3, ak_kose_4, ak_kose_5, ak_kose_6, ak_kose_7, ak_kose_8, ak_kose_9, ak_kose_10, ak_kose_11],
+        image: [ak_kose_1, ak_kose_2, ak_kose_3, ak_kose_4],
         category: "Koltuk",
         subCategory: "Topwear",
         sizes: ["S", "M", "L"],
@@ -198,7 +108,7 @@ export const products = [
         name: "Assa Mobilya",
         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
         price: 100,
-        image: [assa_1, assa_2, assa_3, assa_4, assa_5, assa_6, assa_7, assa_8, assa_9, assa_10, assa_11, assa_12],
+        image: [assa_1, assa_2, assa_3, assa_4],
         category: "Koltuk",
         subCategory: "Topwear",
         sizes: ["S", "M", "L"],
@@ -210,7 +120,7 @@ export const products = [
         name: "Aydın Mobilya",
         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
         price: 100,
-        image: [aydinmobilya_1, aydinmobilya_2, aydinmobilya_3, aydinmobilya_4, aydinmobilya_5, aydinmobilya_6, aydinmobilya_7, aydinmobilya_8, aydinmobilya_9, aydinmobilya_10, aydinmobilya_11, aydinmobilya_12],
+        image: [aydinmobilya_1, aydinmobilya_2, aydinmobilya_3, aydinmobilya_4],
         category: "Koltuk",
         subCategory: "Topwear",
         sizes: ["S", "M", "L"],
@@ -222,7 +132,7 @@ export const products = [
         name: "Ayens",
         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
         price: 100,
-        image: [ayens_1, ayens_2, ayens_3, ayens_4, ayens_5, ayens_6, ayens_7, ayens_8, ayens_9, ayens_10, ayens_11, ayens_12, ayens_13],
+        image: [ayens_1, ayens_2, ayens_3, ayens_4],
         category: "Koltuk",
         subCategory: "Topwear",
         sizes: ["S", "M", "L"],
@@ -234,7 +144,7 @@ export const products = [
         name: "Bohem",
         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
         price: 100,
-        image: [bohem_1, bohem_2, bohem_3, bohem_4, bohem_5, bohem_6, bohem_7, bohem_8, bohem_9],
+        image: [bohem_1, bohem_2, bohem_3, bohem_4],
         category: "Koltuk",
         subCategory: "Topwear",
         sizes: ["S", "M", "L"],
@@ -246,31 +156,7 @@ export const products = [
         name: "Latte Koltuk Takımı",
         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
         price: 100,
-        image: [latte_1, latte_2, latte_3, latte_4, latte_5, latte_6, latte_7, latte_8, latte_9, latte_10],
-        category: "Koltuk",
-        subCategory: "Topwear",
-        sizes: ["S", "M", "L"],
-        date: 1716634345448,
-        bestseller: true
-    },
-    {
-        _id: "aaah",
-        name: "Mobelax Koltuk Takımı",
-        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
-        price: 100,
-        image: [mobelax_1, mobelax_2, mobelax_3, mobelax_4, mobelax_5, mobelax_6, mobelax_7, mobelax_8, mobelax_9, mobelax_10, mobelax_11, mobelax_12, mobelax_13],
-        category: "Koltuk",
-        subCategory: "Topwear",
-        sizes: ["S", "M", "L"],
-        date: 1716634345448,
-        bestseller: true
-    },
-    {
-        _id: "aaai",
-        name: "Odansse Koltuk Takımı",
-        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
-        price: 100,
-        image: [odansse_1, odansse_2, odansse_3, odansse_4, odansse_5, odansse_6, odansse_7, odansse_8, odansse_9, odansse_10, odansse_11, odansse_12, odansse_13, odansse_14],
+        image: [latte_1, latte_2, latte_3, latte_4],
         category: "Koltuk",
         subCategory: "Topwear",
         sizes: ["S", "M", "L"],
@@ -282,7 +168,7 @@ export const products = [
         name: "Sandi Koltuk Takımı",
         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
         price: 100,
-        image: [sandi_1, sandi_2, sandi_3, sandi_4, sandi_5, sandi_6, sandi_7, sandi_8],
+        image: [sandi_1, sandi_2, sandi_3, sandi_4],
         category: "Koltuk",
         subCategory: "Topwear",
         sizes: ["S", "M", "L"],
@@ -294,7 +180,7 @@ export const products = [
         name: "Tree Mobilya",
         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
         price: 100,
-        image: [tree_1, tree_2, tree_3, tree_4, tree_5, tree_6, tree_7, tree_8, tree_9, tree_10, tree_11, tree_12, tree_13, tree_14],
+        image: [tree_1, tree_2, tree_3, tree_4],
         category: "Koltuk",
         subCategory: "Topwear",
         sizes: ["S", "M", "L"],
--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,47 +1,47 @@
 import { createContext, useEffect, useState } from "react";
-import axios from 'axios'
+import axios from 'axios';
+import { toast, ToastContainer } from 'react-toastify'; // toast import edildi
+import 'react-toastify/dist/ReactToastify.css';          // css import edildi
 
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
-
     const currency = '₺';
     const delivery_fee = 10;
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
-    const [search, setSearch] = useState('')
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const [search, setSearch] = useState('');
     const [products, setProducts] = useState([]);
     const [showSearch, setShowSearch] = useState(false);
 
     const getProductsData = async () => {
         try {
-
-            const response = await axios.get(backendUrl + '/api/product/list')
+            const response = await axios.get(backendUrl + '/api/product/list');
             if(response.data) {
-                setProducts(response.data.products)
+                setProducts(response.data.products);
             } else {
-                toast.error(response.data.message)
+                toast.error(response.data.message);
             }
-
         } catch (error) {
-            console.log(error)
-            toast.error(error.message)
+            console.log(error);
+            toast.error(error.message);
         }
-    }
+    };
 
     useEffect(() => {
-        getProductsData()
-    },[])
+        getProductsData();
+    }, []);
 
     const value = {
         products, currency, delivery_fee,
         search, setSearch, showSearch, setShowSearch, backendUrl
-    }
+    };
 
     return (
         <ShopContext.Provider value={value}>
             {props.children}
+            <ToastContainer /> {/* Burada ToastContainer render edildi */}
         </ShopContext.Provider>
-    )
-}
+    );
+};
 
 export default ShopContextProvider;
